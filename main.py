8c297ea3f599d9e642227db61f93a5e6dd3cdbe8
oshan691 <oshannirmal488@gmail.com>
2026-03-27 16:10:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ st.markdown("""
 try:
     api_key = st.secrets["GEMINI_API_KEY"]
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-2.5-flash')
 except:
     st.error("කරුණාකර Gemini API Key එක Secrets වල ඇතුළත් කරන්න.")
 
